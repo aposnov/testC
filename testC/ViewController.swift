1c81dd764d805c0c2be7d7609a5d16a92ad0506d
Andrey Posnov <aposnov@wakeapp.ru>
2019-12-26 00:29:12 +0300

    Initial Commit

---
+++ testC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testC
+//
+//  Created by Andrey Posnov on 26.12.2019.
+//  Copyright © 2019 Andrey Posnov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
